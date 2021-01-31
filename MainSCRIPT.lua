42a4563436d7577a2788290338c8332762b973d3
GunSlingerEAC <55584920+GunSlingerEAC@users.noreply.github.com>
2021-01-31 14:42:00 -0700

    Update MainSCRIPT.lua

--- MainSCRIPT.lua
+++ MainSCRIPT.lua
@@ -312,13 +312,7 @@ NoRecoilZTOGGLEKEYONCEPRESSEDWHENWEAPONOUT.TextColor3 = Color3.fromRGB(0, 0, 0)
 NoRecoilZTOGGLEKEYONCEPRESSEDWHENWEAPONOUT.TextScaled = true
 NoRecoilZTOGGLEKEYONCEPRESSEDWHENWEAPONOUT.TextSize = 14.000
 NoRecoilZTOGGLEKEYONCEPRESSEDWHENWEAPONOUT.TextWrapped = true
-NoRecoilZTOGGLEKEYONCEPRESSEDWHENWEAPONOUT.MouseButton1Down:connect(function()
-game:GetService("UserInputService").InputBegan:connect(function (input)
-if input.KeyCode==Enum.KeyCode.Z then
-getgenv().Toggle = true
-getgenv().ValueCheck = true
-loadstring(game:HttpGet(('https://raw.githubusercontent.com/yesok3877/Miscellaneous/master/Universal-No-Recoil'),true))()
-end
+
 end)
 
 -- Scripts:
